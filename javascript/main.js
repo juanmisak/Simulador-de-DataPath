8bcdfcf421b7f0eea7901df214b4f3b00464fb43
Juan Mite <juanmisak@hotmail.com>
2013-08-13 13:28:42 -0500

    correct order of components and add the combo box solutions

--- javascript/main.js
+++ javascript/main.js
@@ -17,27 +17,27 @@ function loadMedia(){
 	fondo = new Image();
 	fondo.src='fondos/aa.jpg';
 	fondo.onload = function(){
-	 var intervalo = window.setInterval(frameLoop,1000/55);}
+	 var intervalo = window.setInterval(frameLoop,5000/55);}
 	 
 	IM = new Image();
 	IM.src='fondos/IM.jpg';
 	IM.onload = function(){
-	 var intervalo = window.setInterval(frameLoop,1000/55);}
+	 var intervalo = window.setInterval(frameLoop,5000/55);}
 	 
 	DM= new Image();
 	DM.src='fondos/DM.jpg';
 	DM.onload = function(){
-	 var intervalo = window.setInterval(frameLoop,1000/55);}
+	 var intervalo = window.setInterval(frameLoop,5000/55);}
 
 	REG= new Image();
 	REG.src='fondos/REG.jpg';
 	REG.onload = function(){
-	 var intervalo = window.setInterval(frameLoop,1000/55);}
+	 var intervalo = window.setInterval(frameLoop,5000/55);}
 	 
 	ALU = new Image();
 	ALU.src='fondos/ALU.jpg';
 	ALU.onload = function(){
-	 var intervalo = window.setInterval(frameLoop,1000/55);}
+	 var intervalo = window.setInterval(frameLoop,5000/55);}
 
 }
 
@@ -48,9 +48,12 @@ function drawBackground() {
 function simular(){
 	for(var i in disparos){
 		var disparo = disparos[i];
-		disparo.x +=0.3;
-		disparo.y -=0.3;
+		disparo.x +=0.4;
+		disparo.y -=0.5;
+
 	}
+
+	
 	disparos = disparos.filter(function(disparo){
 		return disparo.x < 940;
 	});
@@ -72,11 +75,11 @@ function dibujarDisparos() {
     ctx.save();
     for (var i in disparos) {
         var disparo = disparos[i];
-        ctx.drawImage(IM,disparo.x,disparo.y-30);
-		ctx.drawImage(REG,disparo.x+70,disparo.y-30);
+        ctx.drawImage(REG,disparo.x,disparo.y-30);
+		ctx.drawImage(DM,disparo.x+70,disparo.y-30);
 		ctx.drawImage(ALU,disparo.x+140,disparo.y-30);
-		ctx.drawImage(DM,disparo.x+210,disparo.y-30);
-		ctx.drawImage(REG,disparo.x+280,disparo.y-30);			
+		ctx.drawImage(REG,disparo.x+210,disparo.y-30);
+		ctx.drawImage(IM,disparo.x+280,disparo.y-30);			
 	}
 	ctx.restore();
 }
@@ -104,11 +107,7 @@ function agregarEventosTeclado() {
 }
 
 function moverNave() {
-    if (teclado[38]) {
-        nave.y -= 6;
-        if (nave.y < 80)
-            nave.y = 60;
-    }
+
     if (teclado[32]) {
         var limite = 420;
         nave.y += 10;
@@ -125,12 +124,6 @@ function moverNave() {
         teclado.tiro = false;
 }
 
-function dibujarNave(){
-	ctx.save();
-	ctx.fillStyle ='black';
-	ctx.fillRect(nave.x, nave.y, nave.width, nave.height);
-	ctx.restore();
-}
 
 var el = document.getElementById("play");
 el.addEventListener("click",dibujarDisparos, false);
@@ -140,6 +133,7 @@ function frameLoop(){
 	moverNave();
 	simular();
 	drawBackground();
+	
 	dibujarDisparos();
 	dibujarNave();
 }
