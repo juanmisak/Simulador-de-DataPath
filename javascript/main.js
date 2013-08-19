8a54960d33112b306b994a0351f3896a0fb178b1
Juan Mite <juanmisak@hotmail.com>
2013-08-18 19:18:37 -0500

    hazards identificated now

--- javascript/main.js
+++ javascript/main.js
@@ -3,15 +3,14 @@
 var canvas = document.getElementById('juegoAtari');
 var ctx = canvas.getContext('2d');
 var fondo;
-var teclado={};
-var disparosEnemigos=[];
+
+
 var disparos = [];
-var enemigos = [];
 var nave = {x:25,y:550,width:1,height:1};
-var juego = { estado : 'iniciando'};
+
 var R = ["add","sub","mul"];
-var I = [];
-var J = [];
+var I = ["sw","lw"];
+
 
 function loadMedia(){
 	fondo = new Image();
@@ -55,24 +54,27 @@ function simular(){
 
 	
 	disparos = disparos.filter(function(disparo){
-		return disparo.x < 940;
+		return disparo.x < 900;
 	});
     disparos = disparos.filter(function (disparo) {
         return disparo.y > 70;
 	});
+	
 }
 
-function tiro(){
+function tiro(n){
+	for(i=0 ; i<n;  i++){
 	disparos.push({
-	x:nave.x +10,
-	y:nave.y -15,
+	x:nave.x - i*70,
+	y:nave.y -145+70*i,
 	width: 50,
 	height: 50
 	});	
 }
-
+}
 function dibujarDisparos() {
     ctx.save();
+    
     for (var i in disparos) {
         var disparo = disparos[i];
         ctx.drawImage(REG,disparo.x,disparo.y-30);
@@ -82,62 +84,97 @@ function dibujarDisparos() {
 		ctx.drawImage(IM,disparo.x+280,disparo.y-30);			
 	}
 	ctx.restore();
-}
-
-function agregarEventosTeclado() {
-	 agregarEvento(document,'keydown',function(e){
-	 teclado[e.keyCode]=true;
-	 });
-	 
-	 agregarEvento(document.getElementById("play"),'click',dibujarDisparos());
-	 
-	 agregarEvento(document,'keyup',function(e){
-	 teclado[e.keyCode]=false;
-	 });
-	 function agregarEvento(elemento, nombreEvento, funcion) {
-	     if (elemento.addEventListener) {
-	         //todos menos explorer
-             elemento.addEventListener(nombreEvento,funcion,false);
-	}
-	else if(elemento.attachEvent){
-	//Internet explorer
-	elemento.attachEvent(nombreEvento,funcion);
-	}
-	}
-}
-
-function moverNave() {
-
-    if (teclado[32]) {
-        var limite = 420;
-        nave.y += 10;
-        if (nave.y > limite)
-            nave.y = limite;
-    }
-    if (teclado[32]) {
-        if (!teclado.tiro) {
-            tiro();
-            teclado.tiro = true;
-        }
-    }
-    else
-        teclado.tiro = false;
+	
+	
 }
 
 
-var el = document.getElementById("play");
-el.addEventListener("click",dibujarDisparos, false);
+function play() {
+	n=0;
+	
+	for(i=0;i<10;i++){
+	s=document.getElementById("instruccion"+i);	
+	if(s.value != "")
+	n++;
+	if (n==0){
+	alert("Aún no has ingresado ninguna instrucción MIPS");
+	return null;}
+	}
+	
+	
+	e = document.getElementById("etapas");
+	s = e.options[e.selectedIndex].value;
+	if(s==2)
+	hazard(n);
+	if(s==3)
+	nop(n);
+	
+	
+	
+for(i=0;i<n;i++){
+	bandera = true;
+	s=document.getElementById("instruccion"+i);
+	for(j=0;j<R.length;j++)	
+		if(s.value.toLowerCase() == R[j])
+		bandera = false
+	if(bandera){
+		dialog("¿"+ s.value+"? ¡Esa NO es una instrucción MIPS!  ");
+		return null;
+	}
+	tiro(n);
+}
+}
+
+function pause() {
+	alert("¡Se ha pausado la simulación! Para continuar presione Aceptar");
+	
+}
+
+function stop() {
+	disparos = null;
+	alert("¡Se ha parado la simulación!");
+	
+}
+
+function nop(n) {
+	cont =0;
+	for(j=0;j<n;j++){
+	if(n!=9){
+	s=document.getElementById("registroDestino"+j);
+	d=document.getElementById("registro"+(j+1)+""+1);
+	d2=document.getElementById("registro"+(j+1)+""+2);
+	//alert(" "+s.value+" "+ d.value +" " +d2.value);
+	if(s.value == d.value || s.value == d2.value)
+	cont++;
+	
+	}}
+	alert("Se van a resolver "+cont +" hazard(s)!!!");
+	
+	
+}	
+function hazard(n) {
+	cont =0;
+	for(j=0;j<n;j++){
+	if(n!=9){
+	s=document.getElementById("registroDestino"+j);
+	d=document.getElementById("registro"+(j+1)+""+1);
+	d2=document.getElementById("registro"+(j+1)+""+2);
+	//alert(" "+s.value+" "+ d.value +" " +d2.value);
+	if(s.value == d.value || s.value == d2.value)
+	cont++;
+	
+	}}
+	alert("Hay "+cont +" hazard(s)!!!");
+}
 
 
 function frameLoop(){
-	moverNave();
+
 	simular();
 	drawBackground();
-	
 	dibujarDisparos();
-	dibujarNave();
-}
-
-agregarEventosTeclado();
+	
+	}
 
 loadMedia();
+		
